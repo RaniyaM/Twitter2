edefa35c7c224d65fd28af6ae15cff096ed50cb6
RaniyaM <raniyamillsap@gmailcom>
2020-09-25 20:23:31 -0500

    twitter

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -43,8 +43,14 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
         return tweets.size();
     }
 
-    public void addAll(List<Tweet> tweets) {
+    public void clear() {
+        tweets.clear();
+        notifyDataSetChanged();
+    }
 
+    public void addAll(List<Tweet> tweetList) {
+        tweets.addAll(tweetList);
+        notifyDataSetChanged();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
